0c7ab97b249503015aed92f2256b5b7b706ab014
Scott Francis <scott.francis@shopify.com>
2015-02-22 16:31:32 -0500

    Problem 3

--- src/project_euler/core.clj
+++ src/project_euler/core.clj
@@ -11,6 +11,17 @@
 				(recur (+ sum i) (inc i))
 				(recur sum (inc i))))))
 
+(defn problem_3
+	"https://projecteuler.net/problem=3"
+	[]
+	(loop [n 600851475143 p 2 factors []]
+		(if (= n 1)
+			(last factors)
+			(if (= (mod n p) 0)
+				(recur (/ n p) p (conj factors p))
+				(recur n (+ p 1) factors)))))
+
+
 (defn -main
   "Project Euler"
   [& args]
